2443d8a60edf562a5e9417613a126fd63f247411
bredacoder <bredacoder@gmail.com>
2023-10-29 19:54:20 +0000

    chore: initial dependencies configuration

---
+++ jest-integration-configs.js
@@ -0,0 +1,3 @@
+const config = require('./jest.config') //eslint-disable-line
+config.testMatch = ['**/*.test.ts']
+module.exports = config
---
+++ jest-unit-config.js
@@ -0,0 +1,3 @@
+const config = require('./jest.config') //eslint-disable-line
+config.testMatch = ['**/*.spec.ts']
+module.exports = config
